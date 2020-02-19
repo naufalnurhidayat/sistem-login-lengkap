0cbe1e80360e49d228416e6eeb311321dc7cd2f8
Naufal Nur Hidayat <naufal.gen13@gmail.com>
2020-02-19 14:48:22 +0700

    Mencegah login untuk orang lain

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -119,7 +119,7 @@ class Auth extends CI_Controller {
     		'protocol' => 'smtp',
     		'smtp_host' => 'ssl://smtp.googlemail.com',
     		'smtp_user' => 'naufalnurhidayat510@gmail.com',
-    		'smtp_pass' => 'utamadepok',
+    		'smtp_pass' => 'naufalnurhidayat',
     		'smtp_port' =>  465,
     		'mailtype' => 'html',
     		'charset' => 'utf-8',
